d3cd45d6b48bec6b09f14c3a90f603453934222b
Timileyin Odegbile <odegbiletimileyin11@gmail.com>
2023-01-17 01:29:35 +0100

    Wrap up

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const KEY = 'AIzaSyBUqkQLSm1Gs7rkthOpEiu6tn1OX77NNp0';
+const KEY = "AIzaSyBUqkQLSm1Gs7rkthOpEiu6tn1OX77NNp0";
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3', 
-        params: {
-            part: 'snippet',
-            maxResults: 5,
-            key: KEY
-        }
-    }
-)
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
